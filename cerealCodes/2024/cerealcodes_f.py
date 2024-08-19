f02a90ab6366ae9a52ee54e33a07f69cce2ac33e
Daniel Umemezie <danielu.build@gmail.com>
2024-08-19 16:16:44 -0500

    Create cerealcodes_f.py

---
+++ cerealCodes/2024/cerealcodes_f.py
@@ -0,0 +1,20 @@
+// Credits by Saketh
+
+from collections import defaultdict
+
+def count_valid_pairs(n, a):
+    diff_count = defaultdict(int)
+    pair_count = 0
+    
+    for i, value in enumerate(a):
+        diff = value - (i + 1)  
+        pair_count += diff_count[diff]
+        diff_count[diff] += 1
+    
+    return pair_count
+
+n = int(input())
+a = list(map(int, input().split()))
+
+result = count_valid_pairs(n, a)
+print(result)
