28acb961c06a25b490e612082087938259a83ca6
Daniel Umemezie <danielu.build@gmail.com>
2024-08-19 16:09:13 -0500

    Create cerealcodes_d.py
    Code by Saketh Baddam

---
+++ cerealCodes/2024/cerealcodes_d.py
@@ -0,0 +1,18 @@
+def construct_grid(n, k):
+    grid = [['0'] * n for _ in range(n)]
+    
+    full_rows = k // n  
+    for i in range(full_rows):
+        grid[i] = ['1'] * n
+    
+    remaining = k - (full_rows * n)
+    if remaining > 0:
+        grid[full_rows][:remaining] = ['1'] * remaining
+    
+    return [''.join(row) for row in grid]
+
+n, k = map(int, input().split())
+
+result = construct_grid(n, k)
+for row in result:
+    print(row)
