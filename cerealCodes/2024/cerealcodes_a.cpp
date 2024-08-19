9f3205c1cca1d76a661d405ebea4e12a41b2cd7e
Daniel Umemezie <danielu.build@gmail.com>
2024-08-19 16:15:28 -0500

    Update cerealcodes_a.cpp
    Formatted code

--- cerealCodes/2024/cerealcodes_a.cpp
+++ cerealCodes/2024/cerealcodes_a.cpp
@@ -4,11 +4,21 @@
 using namespace std;
 
 int main() {
-  int n = 0;
-  cin>>n;
-  if(n==2)cout<<69;
-  else if(n==3)cout<<111;
-  else if(n==4)cout<<1221;
-  else if(n==5)cout<<60009;
-  else cout<<126921;
+    int n = 0;
+    cin >> n;
+
+    if (n == 2) {
+        cout << 69;
+    } else if (n == 3) {
+        cout << 111;
+    } else if (n == 4) {
+        cout << 1221;
+    } else if (n == 5) {
+        cout << 60009;
+    } else {
+        cout << 126921;
+    }
+
+    return 0;
 }
+
