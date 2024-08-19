8d28ba1f11b375311893fa55be944917ade6caf7
Daniel Umemezie <danielu.build@gmail.com>
2024-08-19 16:12:57 -0500

    Create cerealcodes_e.cpp
    Code by Helen Peng

---
+++ cerealCodes/2024/cerealcodes_e.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int loops;
+    cin >> loops;
+
+    for (int l = 0; l < loops; l++) {
+        int n, k;
+        cin >> n >> k;
+
+        int count = 0;
+        for (int j = 0; j < k; j++) {
+            int one, two;
+            cin >> one >> two;
+
+            if (abs(one - two) % 2 != 1) {
+                count++;
+            }
+        }
+        cout << count << endl;
+    }
+
+    return 0;
+}
